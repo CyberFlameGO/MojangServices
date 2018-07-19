e7aac4110880c1b1e1d35a157858dbc6b06b5b8d
Eros <george.marr.cobc@gmail.com>
2018-07-19 22:16:23 +0100

    init

--- ServiceHandler.R
+++ ServiceHandler.R
@@ -3,10 +3,10 @@ library(httr)
 
 serviceNames <- c("sessionserver.mojang.com", "authserve.mojang.com", "textures.minecraft.net", "api.mojang.com")
 
-updateServer <- (name, status){
+updateServer <- function(name, status){
   for(in in serviceNames){
     if(name != name[in]){
-      
+      print("Service name is not valid! ( " + name[in] + ")")
     }
   }
 }
