278ab955f6745d1ce143c32b618cd0a10487a290
Eros <george.marr.cobc@gmail.com>
2018-07-19 22:26:31 +0100

    addds in status check

--- ServiceHandler.R
+++ ServiceHandler.R
@@ -3,10 +3,27 @@ library(httr)
 
 serviceNames <- c("sessionserver.mojang.com", "authserve.mojang.com", "textures.minecraft.net", "api.mojang.com")
 
+state <- {
+  # does nothing yet
+}
+
+startTime <- { Sys.time() }
+
 updateServer <- function(name, status){
   for(in in serviceNames){
     if(name != name[in]){
       print("Service name is not valid! ( " + name[in] + ")")
+    } else {
+      status <- ""
+      if(GET(name)[status] == "OK"){
+        state = "Online"
+      } else if(GET(name)[status] == "UNSTABLE"){
+        state = "Unstable"
+      } else if(GET(name)[status] == "OFFLINE"){
+        state = "Offline"
+      } else {
+        state = "Cannot retrieve information"
+      }
     }
   }
 }
