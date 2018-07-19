7c508ae806fe29d2d367bd0a2d52eb01412a0f36
Eros <george.marr.cobc@gmail.com>
2018-07-19 22:15:22 +0100

    init

---
+++ ServiceHandler.R
@@ -0,0 +1,12 @@
+install.packages("httr")
+library(httr)
+
+serviceNames <- c("sessionserver.mojang.com", "authserve.mojang.com", "textures.minecraft.net", "api.mojang.com")
+
+updateServer <- (name, status){
+  for(in in serviceNames){
+    if(name != name[in]){
+      
+    }
+  }
+}
